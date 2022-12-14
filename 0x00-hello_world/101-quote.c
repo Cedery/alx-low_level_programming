d73ddabdfc6b04c2aa12e1d2c7eceba9fb4e47ba
Cedery <oniyorjr@gmail.com>
2022-12-14 02:23:32 -0800

    new

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <unistd.h>
 /**
- *main - Entry point 
- *Description: Program that prints (and that piece of art is useful)
+ *main - Entry point
+ *
  *Return: always 1 (success)
  */
 int main(void)
 {
-	char  str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(2, str1, 59);
 	return (1);
 }
