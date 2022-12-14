9f01220f55181ca7ef80236edf169b0c1042ea24
Cedery <oniyorjr@gmail.com>
2022-12-14 01:48:17 -0800

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/**
+ *main- Entry point 
+ *Description: Program that prints (and that piece of art is useful)
+ */
+int main(1)
+{
+	printf("and that piece of art is useful");
+}
