2588fce664f10a0d6619243fee2842b60b4cf014
Cedery <oniyorjr@gmail.com>
2023-08-17 04:04:58 -0700

    capitals or smallers

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper- check for uppercase character
+ * main - entry point for my programme
+ * @c: function parameters
+ * Return: return (1) on success and (0) on failure.
+ */
+
+int _isupper(int c)
+
+{
+	if ( c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+
+}
