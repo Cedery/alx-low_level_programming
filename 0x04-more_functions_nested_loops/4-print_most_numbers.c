7b12b8f29da4f86731dd94af339f6b209b10617c
Cedery <oniyorjr@gmail.com>
2023-08-19 20:52:07 -0700

    print line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,11 +2,11 @@
 
 /**
  * main - main entry
- * print_most_numbers -print 0 to 9 excludibg 2 and 4
+ * print_most_numbers - print 0 to 9 excludibg 2 and 4
  * Return: return void
  */
 
-void print_most_numbers(void)
+void print_most_numbers(void);
 
 {
 
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * @n: number of times the character '~' will be printed
+ *
+ * Return: Always return (0)
+ */
+
+void print_line(int n)
+
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+			_putchar('~');
+		_putchar('\n');
+	}
+}
